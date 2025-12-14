4b3578c24d0f525ce402a80d9e74c7c927c6e1ae
hoainam2407 <hoainamvpm@gmail.com>
2025-12-14 18:01:06 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/landing_page_doc/" : "/",
+  base: "/", // Custom domain: bacsithuy.io.vn
 });
