5d38b24d0d8fdf50e37d26eb207cc2c12acd1baf
hoainam2407 <hoainamvpm@gmail.com>
2025-12-15 20:23:24 +0700

    Add Google Analytics 4 via vite-plugin-html

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        tags: [
+          {
+            injectTo: "head",
+            tag: "script",
+            attrs: {
+              async: true,
+              src: "https://www.googletagmanager.com/gtag/js?id=G-DEXYWPY93E",
+            },
+          },
+          {
+            injectTo: "head",
+            tag: "script",
+            children: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DEXYWPY93E');
+            `,
+          },
+        ],
+      },
+    }),
+  ],
   base: "/", // Custom domain: bacsithuy.io.vn
 });
