031eafe218f57189d9ff26c1fa8b6572fd04f61d
hoainam2407 <hoainamvpm@gmail.com>
2025-12-15 20:36:23 +0700

    Add GA4 directly to index.html body - remove plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    createHtmlPlugin({
-      minify: true,
-      inject: {
-        tags: [
-          {
-            injectTo: "head",
-            tag: "script",
-            attrs: {
-              async: true,
-              src: "https://www.googletagmanager.com/gtag/js?id=G-DEXYWPY93E",
-            },
-          },
-          {
-            injectTo: "head",
-            tag: "script",
-            children: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-DEXYWPY93E');
-            `,
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [vue()],
   base: "/", // Custom domain: bacsithuy.io.vn
 });
